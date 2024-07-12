f4c38854d6a422a20f88d076b6461f9623be8e69
RedJunKi <bing8821@naver.com>
2024-07-12 16:33:20 +0900

    solve 1654

--- src/LanCableCut_BinarySearch_1654.java
+++ src/LanCableCut_BinarySearch_1654.java
@@ -1,10 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class LanCableCut_BinarySearch_1654 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());

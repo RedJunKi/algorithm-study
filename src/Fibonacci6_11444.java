c216451f1f02c90e42e1a3add95a94f528c33321
RedJunKi <bing8821@naver.com>
2024-07-14 21:51:49 +0900

    solve 11444 (다시 봐야함)

--- src/Fibonacci6_11444.java
+++ src/Fibonacci6_11444.java
@@ -1,10 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Fibonacci6_11444 {
     private static int MOD = 1000000007;
 
     public static void main(String[] args) throws IOException {
